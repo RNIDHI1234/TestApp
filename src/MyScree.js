b691bbaf7462df57d8db609e909d955e7aa9c8d1
your_username <raja.kumar@only4values.com>
2023-09-05 23:41:34 +0530

    code push

---
+++ src/MyScree.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+
+const MyScree = ()=>{
+    return(
+        <View style={{backgroundColor:"yellow"}}>
+            <Text>Hello Kajal!</Text>
+        </View>
+    )
+}
+
+export default MyScree;
